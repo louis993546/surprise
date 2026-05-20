6d2d7376a582a320693f7217ad822043c61fb9b6
Louis <louis993546@gmail.com>
2026-05-20 21:07:50 +0200

    Clean up

--- app/src/main/java/de/berlindroid/mario/pages/ImageDemoPage.kt
+++ app/src/main/java/de/berlindroid/mario/pages/ImageDemoPage.kt
@@ -3,7 +3,6 @@ package de.berlindroid.mario.pages
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -12,7 +11,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import coil3.compose.AsyncImage
-import coil3.compose.SubcomposeAsyncImage
 import de.berlindroid.mario.di.AppScope
 import de.berlindroid.mario.model.Page
 import dev.zacsweers.metro.ContributesIntoSet
@@ -36,12 +34,9 @@ object ImageDemoPage : Page {
     @Composable
     override fun RightContent() {
         Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-            SubcomposeAsyncImage(
+            AsyncImage(
                 model = "https://picsum.photos/seed/mario/800/600",
                 contentDescription = "Demo Image",
-                loading = {
-                    CircularProgressIndicator()
-                },
                 modifier = Modifier.fillMaxSize(),
                 contentScale = ContentScale.Crop
             )
